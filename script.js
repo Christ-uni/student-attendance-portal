984ccda8eb2bbd9123bb9a831a396442e86aafa4
Christ-uni <ryder.nyt20@gmail.com>
2025-11-21 13:44:54 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+// Login validation
+function login() {
+    const correctId = "2412205";
+    const correctPassword = "38716777";
+
+    let enteredId = document.getElementById("studentId").value;
+    let enteredPassword = document.getElementById("password").value;
+
+    if (enteredId === correctId && enteredPassword === correctPassword) {
+        // Hide login and show attendance
+        document.querySelector(".login-box").style.display = "none";
+        document.querySelector(".attendance-box").style.display = "block";
+    } else {
+        alert("Incorrect Login ID or Password.");
+    }
+}
+
+// Logout
+function logout() {
+    location.reload();
+}
