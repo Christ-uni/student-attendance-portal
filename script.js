1c0961ad48290f4b78a00ccf74cddd6b04d87b4f
Christ-uni <ryder.nyt20@gmail.com>
2025-11-21 15:28:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,27 @@
-// Student stored data (can be expanded later)
-const student = {
-  id: "2412205",
-  password: "38716778",
-  name: "Ananya Nallurmana Jayadevan",
-  email: "ananya.jayadevan@bcomfah.christuniversity.in",
-  rollno: "2412205",
-  class: "4BCOMF&A C",
-  phone: "9606357445"
-};
-
 function login() {
-  let username = document.getElementById("username").value;
-  let password = document.getElementById("password").value;
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
 
-  if (username === student.id && password === student.password) {
-    document.getElementById("message").textContent = "Login Success!";
-    document.getElementById("message").style.color = "green";
+    if(username === "2412205" && password === "38716778") {
 
-    // Store login data in browser storage
-    localStorage.setItem("studentData", JSON.stringify(student));
+        localStorage.setItem("studentData", JSON.stringify({
+            name: "Ananya Nallurmana Jayadevan",
+            email: "ananya.jayadevan@bcomfah.christuniversity.in",
+            roll: "2412205",
+            class: "4BCOMF&A C",
+            phone: "9606357445"
+        }));
 
-    // Redirect to profile
-    setTimeout(() => {
-      window.location.href = "profile.html";
-    }, 1000);
+        document.getElementById("message").textContent = "Login Successful!";
+        document.getElementById("message").style.color = "green";
 
-  } else {
-    document.getElementById("message").textContent = "Invalid login, try again.";
-    document.getElementById("message").style.color = "red";
-  }
+        setTimeout(() => {
+            window.location.href = "attendance.html";
+        }, 900);
+
+    } else {
+        document.getElementById("message").textContent = "Invalid Login Details!";
+        document.getElementById("message").style.color = "red";
+    }
 }
+
